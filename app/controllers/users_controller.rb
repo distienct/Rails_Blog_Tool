589c3d23fd5bf92589e786d55964850aafa3062e
distienct <heytnguyen@gmail.com>
2015-07-08 18:24:45 -0700

    fixing spacing issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :avatar, :password, :password_confirmation)
+		testing
+			testing
   end
 
 end
